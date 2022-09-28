168cab481293b077ba0ecf41d60292c23eed2832
Ana Paula <aninha10.desouza@gmail.com>
2022-09-28 15:56:45 -0300

    ok

--- livraria/settings.py
+++ livraria/settings.py
@@ -115,4 +115,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
