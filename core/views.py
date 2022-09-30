9b5068bce48967d8e97889bebdf76e0015a575cc
Ana Paula <aninha10.desouza@gmail.com>
2022-09-30 13:46:13 -0300

    ok

--- core/views.py
+++ core/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
+from django.views import View
+from core.models import Categoria
+
+import json 
+
 
 def teste (request):
     return HttpResponse("Olá Mundo do Django.")
 
 def teste2 (request):
     return HttpResponse("uma nova página")
+
+class CategoriaView(View):
+    def get(self, request):
+        data = list(Categoria.objects.values())
+        formatted_data = json.dumps(data, ensure_ascii=False)
+        return HttpResponse(formatted_data, content_type="application/json")
