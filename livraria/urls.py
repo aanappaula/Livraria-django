3c603c0e5d61afc364a65cdbaa1fc57ac16b20fb
Ana Paula <aninha10.desouza@gmail.com>
2022-10-07 10:44:56 -0300

    ok

--- livraria/urls.py
+++ livraria/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('teste/', views.teste),
     path('pag/', views.teste2),
     path('categorias/', views.CategoriaView.as_view()),
-    path('categorioas/<int:id>/', views.CategoriaView.as_view())
+    path('categorias/<int:id>/', views.CategoriaView.as_view())
 ]
 
