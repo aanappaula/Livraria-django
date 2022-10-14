9ffc9975d448713c240d2e648810d9db90b44f75
Ana Paula <aninha10.desouza@gmail.com>
2022-10-14 13:44:11 -0300

    ok

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
+from django.shortcuts import get_object_or_404
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
@@ -74,3 +75,25 @@ class CategoriasList(APIView):
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CategoriaDetail(APIView):
+    def get(self, request, id):
+        categoria = get_object_or_404(Categoria.objects.all(), id=id)
+        serializer = CategoriaSerializer(categoria)
+        return Response(serializer.data)
+
+    def put (self, request, id):
+        categoria = get_object_or_404(Categoria.objects.all(), id=id)
+        serializer = CategoriaSerializer(categoria, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, id):
+        categoria = get_object_or_404(Categoria.objects.all(), id=id)
+        categoria.delet()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+
--- livraria/urls.py
+++ livraria/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('pag/', views.teste2),
     path('categorias/', views.CategoriaView.as_view()),
     path('categorias/<int:id>/', views.CategoriaView.as_view()),
-    path('categorias-apiview/', views.CategoriasList.as_view())
+    path('categorias-apiview/', views.CategoriasList.as_view()),
+    path('categorias-apiview/int:id>/', views.CategoriaDetail.as_view())
 ]
 
