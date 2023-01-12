3d366a05eaef7f249dc49c1e3d49f4b3d91ac75c
Ana Paula <aninha10.desouza@gmail.com>
2023-01-11 21:21:39 -0300

    aula 24, arrumar erro do core views

---
+++ core/views/_init_.py
@@ -0,0 +1,5 @@
+from .testesiIniciais import teste, teste2
+from .categoriasClass import CategoriaView
+from .categoriaApi import CategoriaDetail, CategoriasList
+from .categoriaGeneric import CategoriaDetailGeneric, CategoriasListGeneric
+from .categoria import CategoriaViewSet
